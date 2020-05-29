bfc865c83e27b53621f68ce24a993513fb19218d
vs.kozh@list.ru <vs.kozh@list.ru>
2020-05-29 13:32:51 +0300

    Добавьте файлы проекта.

---
+++ Troelsen_7.0/Chapter_3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Troelsen_7._0
+{
+    /// <summary>
+    /// Класс описывающий программы главы 3 книги Троельсена
+    /// </summary>
+    public  class Chapter_3
+    {
+        /// <summary>
+        /// Демонстрация работы псевдонимов объекта и сокращенных наименований
+        /// </summary>
+        public  void ObjectFunctionality()
+        {
+            Console.WriteLine("Chapter_3");
+            Console.WriteLine("=> System.Object Functionality \n");
+
+            // A C# int is really a shorthand for System.Int32,
+            // which inherits the following members from System.Object
+
+            Console.WriteLine("12.GetHashCode() = {0}", 12.GetHashCode());
+            Console.WriteLine("12.Equals(23) = {0}", 12.Equals(23));
+            Console.WriteLine("12.ToString() = {0}", 12.ToString());
+            Console.WriteLine("12.GetType() = {0}", 12.GetType());
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ Troelsen_7.0/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Troelsen_7._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Модуль управления главой 3
+
+            Chapter_3 chapter = new Chapter_3();
+            chapter.ObjectFunctionality();
+
+
+            //
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
