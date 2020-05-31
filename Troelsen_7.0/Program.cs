7cc6e57d0f4919398cd8de1c68233da13c525d8c
Vladimir Kozhevnikov <vs.kozh@list.ru>
2020-05-31 14:14:07 +0300

    Интерфейс взаимодействия с программой

--- Troelsen_7.0/Program.cs
+++ Troelsen_7.0/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,6 +10,100 @@ namespace Troelsen_7._0
     class Program
     {
         static void Main(string[] args)
+        {
+            ExecutePrograms();
+        }
+
+        /// <summary>
+        /// Контроль исполнения программы
+        /// </summary>
+        private static void ExecutePrograms()
+        {
+            ChapterSelector();
+            bool IsExecuted = false;
+            while (!IsExecuted)
+            {
+                Console.WriteLine("Продолжить выполнение прграммы?");
+                Console.Write("1 - ДА; 2 - НЕТ; ваш выбор?: ");
+                bool IsNumber = false;
+                int choiceSelector = 0;
+                while (!IsNumber)
+                {
+                    if (Int32.TryParse(Console.ReadLine(), out choiceSelector))
+                    {
+                        IsNumber = true;
+                    }
+                    else
+                    {
+                        Console.Write("Некорректный ввод данных, требуется ввести 1 или 2. Попробуйте ещё раз: ");
+                    }
+
+                    switch (choiceSelector)
+                    {
+                        case 1:
+                            Console.WriteLine("Программа продолажет работу");
+                            Console.Clear();
+                            Console.WriteLine("Консоль очищена");
+                            ChapterSelector();
+                            break;
+                        case 2:
+                            Console.WriteLine("Программа прекарщает работу");
+                            IsExecuted = true;
+                            Console.ReadLine();
+                            break;
+                        default:
+                            Console.WriteLine("Некорректный ввод данных, требуется ввести 1 или 2. Попробуйте ещё раз.");
+                            break;
+                    }
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Механизм выбора главы
+        /// </summary>
+        private static void ChapterSelector()
+        {
+            Console.WriteLine("Выберите главу для демонстрации работы содеражщихся в ней программ: \n" +
+                "для выбора главы 3 нажмите цифру 3");
+            Console.Write("Ваш выбор?: ");
+            bool IsSelected = false;
+
+            while (!IsSelected)
+            {
+                int chapterSelector = 0;
+                bool IsNumber = false;
+                while (!IsNumber)
+                {
+                    if (Int32.TryParse(Console.ReadLine(), out chapterSelector))
+                    {
+                        IsNumber = true;
+                    }
+                    else
+                    {
+                        Console.Write("Некорректный ввод данных, требуется ввести число, попробуйте ещё раз: ");
+                    }
+                }
+
+                switch (chapterSelector)
+                {
+                    case 3:
+                        RunChapterThree();
+                        IsSelected = true;
+                        break;
+                    default:
+                        Console.Write("Некорректный ввод данных, попробуйте ещё раз: ");
+                        break;
+                }
+
+            }
+        }
+
+        /// <summary>
+        /// Запуск программ, содеержащихся в главе 3
+        /// </summary>
+        private static void RunChapterThree()
         {
             // Модуль управления главой 3
 
@@ -29,12 +124,13 @@ namespace Troelsen_7._0
             chapter_3.StringEquality();
             chapter_3.StringEqualitySpecifyingCompareRules();
             chapter_3.StrigsAreImmutable();
+
             //
 
 
 
-            Console.ReadLine();
-
         }
+
+
     }
 }
