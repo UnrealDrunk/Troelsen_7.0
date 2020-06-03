31eb20ce8d9e37c78fb60d5c9cf5310f7c0760a4
Vladimir Kozhevnikov <vs.kozh@list.ru>
2020-06-03 21:20:53 +0300

    Создан класс, посвященный главе 4

---
+++ Troelsen_7.0/Chapter_4.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Troelsen_7._0
+{
+    /// <summary>
+    /// Класс описывающий программы главы 3 книги Троельсена
+    /// </summary>
+    public class Chapter_4
+    {
+
+
+
+    }
+}
